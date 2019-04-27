b360edc51b154e8f8058d16f7e2cf7b44025a84d
Jacopo Cascioli <jacopo@jacopocascioli.com>
2019-04-27 19:16:56 +0200

    chore/mix: version bump to 1.0.0

--- mix.exs
+++ mix.exs
@@ -4,7 +4,7 @@ defmodule EfestoClient.MixProject do
   def project do
     [
       app: :efesto_client,
-      version: "0.1.0",
+      version: "1.0.0",
       elixir: "~> 1.8",
       start_permanent: Mix.env() == :prod,
       description: description(),
