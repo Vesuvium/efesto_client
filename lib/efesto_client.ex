0cdee56d6130162209021c0861311f40b6e6fc80
Jacopo Cascioli <jacopo@jacopocascioli.com>
2019-04-27 14:07:09 +0200

    feat/efesto_client: add read method

--- lib/efesto_client.ex
+++ lib/efesto_client.ex
@@ -1,5 +1,16 @@
 defmodule EfestoClient do
-  def hello do
-    :world
+  @moduledoc """
+  Client for Efesto
+  """
+  use Tesla
+
+  def read(endpoint) do
+    case Tesla.get(endpoint) do
+      {:ok, response} ->
+        response
+
+      {:error, error} ->
+        error
+    end
   end
 end
--- test/efesto_client_test.exs
+++ test/efesto_client_test.exs
@@ -1,8 +1,18 @@
 defmodule EfestoClientTest do
   use ExUnit.Case
-  doctest EfestoClient
+  import Dummy
 
-  test "greets the world" do
-    assert EfestoClient.hello() == :world
+  test "the read function" do
+    dummy Tesla, [{"get", fn _x -> {:ok, "body"} end}] do
+      result = EfestoClient.read("/endpoint")
+      assert called(Tesla.get("/endpoint"))
+      assert result == "body"
+    end
+  end
+
+  test "the read function when getting an error" do
+    dummy Tesla, [{"get", fn _x -> {:error, "error"} end}] do
+      assert EfestoClient.read("/endpoint") == "error"
+    end
   end
 end
