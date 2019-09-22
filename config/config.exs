04cf939727d130dc69c97e0f93bf9065b66870cf
Jacopo Cascioli <jacopo@jacopocascioli.com>
2019-09-22 20:18:09 +0200

    feat/config: add api_token option

--- config/config.exs
+++ config/config.exs
@@ -1,6 +1,7 @@
 use Mix.Config
 
 config :efesto_client,
-  api_url: {:system, "API_URL", "http://localhost:5000"}
+  api_url: {:system, "API_URL", "http://localhost:5000"},
+  api_token: {:system, "API_TOKEN", "token"}
 
 import_config "#{Mix.env()}.exs"
